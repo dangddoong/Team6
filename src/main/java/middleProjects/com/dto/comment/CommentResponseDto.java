3ce92b1d35a2976c3beaed40ba545ad45d7070ed
dangddoong <dangddoong@gmail.com>
2023-01-02 12:00:54 +0900

    Feat: CommentResponseDto 생성자 추가 / CommentRepository.findAllByBoardIdOrderByModDateAtDesc 추가

--- src/main/java/middleProjects/com/dto/comment/CommentResponseDto.java
+++ src/main/java/middleProjects/com/dto/comment/CommentResponseDto.java
@@ -1,15 +1,25 @@
 package middleProjects.com.dto.comment;
 
 import lombok.Getter;
+import middleProjects.com.entity.Comment;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class CommentResponseDto {
-    private final Long id;
+    private final Long commentId;
     private final String comment;
+    private final String commentWriter;
     private final LocalDateTime createDate;
-    private final  LocalDateTime modDate;
+    private final LocalDateTime modDate;
+    private final Long recommendCount;
 
-
+    CommentResponseDto(Comment comment, String commentWriter){
+        this.commentId = comment.getId();
+        this.comment = comment.getComment();
+        this.commentWriter = commentWriter;
+        this.createDate = comment.getCreateDate();
+        this.modDate = comment.getModDate();
+        this.recommendCount = comment.getRecommendCount();
+    }
 }
