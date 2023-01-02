7125b93acad67b706ec06d12652e6aba70ebb572
dangddoong <dangddoong@gmail.com>
2023-01-02 11:00:55 +0900

    Feat: Comment entity 생성자 추가, Board entity 살려둠

--- src/main/java/middleProjects/com/entity/Comment.java
+++ src/main/java/middleProjects/com/entity/Comment.java
@@ -2,6 +2,7 @@ package middleProjects.com.entity;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import middleProjects.com.dto.comment.CommentRequestDto;
 
 import javax.persistence.*;
 
@@ -17,10 +18,18 @@ public class Comment extends BaseEntity{
     @Column(nullable = false)
     private String comment;
     @Column(nullable = false)
-    private int recommendCount;
-
-//    private Board board;
+    private Long recommendCount;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
+
+    public Comment(String comment, Board board, Member member ){
+        this.comment = comment;
+        this.board = board;
+        this.member = member;
+        // board에 list보고 맞춰가야함. ex) board.getCommentList().add(this);
+    }
 }
