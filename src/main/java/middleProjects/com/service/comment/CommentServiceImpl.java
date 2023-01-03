1cdd7551f7bfecb85c42d9a04e29c0c735482ca9
dangddoong <dangddoong@gmail.com>
2023-01-03 18:51:26 +0900

    댓글 좋아요 관련 로직 초안완성 - [좋아요 기준] 멤버와 단방향 OneToOne / 댓글과 양방향 ManyToOne

--- src/main/java/middleProjects/com/service/comment/CommentServiceImpl.java
+++ src/main/java/middleProjects/com/service/comment/CommentServiceImpl.java
@@ -3,20 +3,26 @@ package middleProjects.com.service.comment;
 import lombok.RequiredArgsConstructor;
 import middleProjects.com.dto.comment.CommentResponseDto;
 import middleProjects.com.entity.Comment;
+import middleProjects.com.entity.CommentRecommendation;
 import middleProjects.com.entity.Member;
 import middleProjects.com.repository.BoardRepository;
+import middleProjects.com.repository.CommentRecommendationRepository;
 import middleProjects.com.repository.CommentRepository;
 import middleProjects.com.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import middleProjects.com.entity.Board;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class CommentServiceImpl implements CommentService{
+public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
     private final BoardRepository boardRepository;
     private final MemberRepository memberRepository;
+    private final CommentRecommendationRepository commentRecommendationRepository;
+
     @Transactional
     @Override
     public CommentResponseDto createComment(Long boardId, String contents, String username) {
@@ -36,6 +42,7 @@ public class CommentServiceImpl implements CommentService{
         commentRepository.save(comment);
         return new CommentResponseDto(comment);
     }
+
     @Transactional
     @Override
     public void deleteComment(Long commentId, String username) {
@@ -43,4 +50,24 @@ public class CommentServiceImpl implements CommentService{
         comment.memberAndCommentWriterEqualCheck(username);
         commentRepository.deleteById(commentId);
     }
+
+    @Transactional
+    @Override
+    public String recommendComment(Long commentId, String username) {
+        Member member = memberRepository.findByUsername(username).orElseThrow(IllegalArgumentException::new);
+        Optional<CommentRecommendation> optionalCommentRecommend = commentRecommendationRepository.findByMemberAndCommentId(member, commentId);
+        if(optionalCommentRecommend.isPresent()){
+            commentRecommendationRepository.delete(optionalCommentRecommend.get());
+            return "댓글 좋아요 취소완료" ;
+        }
+        Comment comment = commentRepository.findById(commentId).orElseThrow(IllegalArgumentException::new);
+        CommentRecommendation commentRecommend = new CommentRecommendation(comment, member);
+        commentRecommendationRepository.save(commentRecommend);
+        return "댓글 좋아요 완료";
+    }
+    // 이런식으로 findBy 메서드 만들어서 코드 정리를 해볼게요.(성현)s
+//    private Board findBoardByBoardId(Long boardId){
+//        return boardRepository.findById(boardId).orElseThrow(IllegalArgumentException::new);
+//    }
 }
+
